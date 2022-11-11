93f7400172cd1e30a19a412b6abde6a58bee9a3a
Mahimai Raja J <mahimairaja3@gmail.com>
2022-11-11 23:16:07 +0530

    Essential camera functions

--- camera-integration/camera.py
+++ camera-integration/camera.py
@@ -6,11 +6,11 @@ camID2 = '/dev/video2'
 camID3 = '/dev/video4'
 camID4 = '/dev/video6'
 
-camera=cv2.VideoCapture(camID, cv2.CAP_V4L2)
 
-def generate_frames():
+
+def generate_frames(camID):
+    camera=cv2.VideoCapture(camID)
     while True:
-           
         ## read the camera frame
         success,frame=camera.read()
         if not success:
@@ -20,52 +20,6 @@ def generate_frames():
             frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-camera2=cv2.VideoCapture(camID2, cv2.CAP_V4L2)
 
-def generate_frames2():
-    while True:
-           
-        ## read the camera frame
-        success,frame=camera2.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-camera3=cv2.VideoCapture(camID3, cv2.CAP_V4L2)
-
-def generate_frames3():
-    while True:
-           
-        ## read the camera frame
-        success,frame=camera3.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-                
-camera4=cv2.VideoCapture(camID4, cv2.CAP_V4L2)
-
-def generate_frames4():
-    while True:
-           
-        ## read the camera frame
-        success,frame=camera4.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
