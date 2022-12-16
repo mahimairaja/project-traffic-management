2adefa7bb97f7fb9e5086f11575d5385fd1ba748
Mahimai Raja J <mahimairaja3@gmail.com>
2022-12-16 06:50:39 +0530

    Integrated csv with graph and backend

--- camera-integration/main.py
+++ camera-integration/main.py
@@ -1,13 +1,11 @@
 from flask import Flask,render_template,Response, request, redirect, url_for, jsonify, make_response
-import cv2
 from camera import *
 from random import random
-import pyrebase
-import time
-import json
+import pyrebase, time, pandas as pd, json, cv2
 from dotenv import load_dotenv as ld
 from os import environ
 from json import loads
+import threading
 
 app=Flask(__name__)
 
@@ -21,32 +19,30 @@ database = firebase.database()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        l1 = request.form.get("lane1")
-        l2 = request.form.get("lane2")  
-        l3 = request.form.get("lane3")
-        l4 = request.form.get("lane4")
-        
-        curr_time = time.strftime("%H:%M:%S", time.localtime())
-        data = {"lane1" : l1, "lane2" : l2, "lane3" : l3, "lane4" : l4}
-        database.child("Signal-01").child(curr_time).set(data)
-        return jsonify(Lane1 = l1, Lane2 = l2, Lane3 = l3, Lane4 = l4 )
+    writeData()
     return render_template("chart.html")
 
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    data1 = [time.strftime("%H:%M:%S", time.localtime()), random() * 100]
-    data2 = [time.strftime("%H:%M:%S", time.localtime()), random() * 100]
-    data3 = [time.strftime("%H:%M:%S", time.localtime()), random() * 100]
-    data4 = [time.strftime("%H:%M:%S", time.localtime()), random() * 100]
+    data = pd.read_csv('data/data.csv')
+    timeData = data['Time'].iloc[-1]
+    y1 = int(data['lane_1'].iloc[-1])
+    y2 = int(data['lane_2'].iloc[-1])
+    y3 = int(data['lane_3'].iloc[-1])
+    y4 = int(data['lane_4'].iloc[-1])
+    data1 = [timeData, y1]
+    data2 = [timeData, y2]
+    data3 = [timeData, y3]
+    data4 = [timeData, y4]
 
     data = list((data1,data2,data3,data4))
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
-@app.route('/dashboard')
-def dash():
+@app.route('/home')
+def home():
+    writeData()
     return render_template("camera.html")
 
 
@@ -66,5 +62,21 @@ def video3():
 def video4():
     return Response(generate_frames(0),mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def writeData():
+    threading.Timer(5.0, writeData).start()   # For every 5 seconds data in retrived ! 
+    data = pd.read_csv('data/data.csv')
+    x = data['Time'].iloc[-1]
+    y1 = int(data['lane_1'].iloc[-1])
+    y2 = int(data['lane_2'].iloc[-1])
+    y3 = int(data['lane_3'].iloc[-1])
+    y4 = int(data['lane_4'].iloc[-1])
+    curr_time = time.strftime("%H:%M:%S", time.localtime())
+    data = {"lane1" : y1, "lane2" : y2, "lane3" : y3, "lane4" : y4}
+    print("---* ---* ---*")
+    print(data)
+    # database.child("Signal-01").child(curr_time).set(data)   # Uncomment to upload to database ! 
+
 if __name__=="__main_":
     app.run(host='0.0.0.0')
+
+
